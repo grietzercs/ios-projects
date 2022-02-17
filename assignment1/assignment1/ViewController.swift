29c3989eff34d6085d486ecbbe44a958b8eabd1d
Colden <grietzer@vcu.edu>
2022-02-17 04:15:16 -0500

    started template

---
+++ assignment1/assignment1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  assignment1
+//
+//  Created by Colden on 2/17/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
